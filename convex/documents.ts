c4ae53f2b324bdb385844613479cb65c67a67f31
kuilysayantan <kuilysayantan@gmail.com>
2024-01-30 21:15:23 +0530

    added settings and search

--- convex/documents.ts
+++ convex/documents.ts
@@ -146,7 +146,6 @@ export const remove = mutation({
 });
 
 
-
 export const getSidebar = query({
   args: {
     parentDocument: v.optional(v.id("documents"))
@@ -199,3 +198,23 @@ export const create = mutation({
     return document;
   }
 });
+
+
+export const getSearch = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if(!identity) throw new Error("Not authenticated");
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+                                  .query("documents")
+                                  .withIndex("by_user", (q)=>q.eq("userId",userId))
+                                  .filter((q)=>q.eq(q.field("isArchived"),false))
+                                  .order("desc")
+                                  .collect()
+                                
+    return documents;
+  }
+})
